75d3b1994ea1bbee23204842d7c3d384064e8dd6
Richard Try <richardtrybox@protonmail.com>
2020-11-01 01:12:03 +0300

    Initial commit

---
+++ branches.lua
@@ -0,0 +1,96 @@
+local large_nodebox = {
+		type = "fixed",
+		fixed = {
+			{-0.375, -0.0625, -0.375, 0.375, 0.375, 0.375},
+			{-0.5, -0.5, -0.5, 0.5, -0.0625, 0.5},
+		}
+	}
+local medium_nodebox = {
+		type = "fixed",
+		fixed = {
+			{-0.25, -0.1875, -0.25, 0.25, 0.25, 0.25},
+			{-0.375, -0.625, -0.375, 0.375, -0.1875, 0.375},
+		}
+	}
+local small_nodebox = {
+		type = "fixed",
+		fixed = {
+			{-0.125, -0.3125, -0.125, 0.125, 0.125, 0.125},
+			{-0.25, -0.75, -0.25, 0.25, -0.3125, 0.25},
+		}
+	}
+local stick_nodebox = {
+		type = "fixed",
+		fixed = {
+			{-0.0625, -0.4375, -0.0625, 0.0625, 0.0625, 0.0625},
+			{-0.125, -0.875, -0.125, 0.125, -0.4375, 0.125},
+		}
+	}
+
+minetest.register_node("living_trees:branch_1", {
+	description = "Large branch",
+	tiles = {
+		{
+		name = "default_tree.png",
+		backface_culling = true,
+		}
+	},
+	drawtype = "mesh",
+	mesh = "trunk_to_large.obj",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	groups = {oddly_breakable_by_hand=2},
+	selection_box = large_nodebox,
+	collision_box = large_nodebox
+})
+
+minetest.register_node("living_trees:branch_2", {
+	description = "Medium branch",
+	tiles = {
+		{
+		name = "default_tree.png",
+		backface_culling = true,
+		}
+	},
+	drawtype = "mesh",
+	mesh = "large_to_medium.obj",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	groups = {oddly_breakable_by_hand=2},
+	selection_box = medium_nodebox,
+	collision_box = medium_nodebox
+})
+
+minetest.register_node("living_trees:branch_3", {
+	description = "Small branch",
+	tiles = {
+		{
+		name = "default_tree.png",
+		backface_culling = true,
+		}
+	},
+	drawtype = "mesh",
+	mesh = "medium_to_small.obj",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	groups = {oddly_breakable_by_hand=2},
+	selection_box = small_nodebox,
+	collision_box = small_nodebox
+})
+
+minetest.register_node("living_trees:branch_4", {
+	description = "Stick branch",
+	tiles = {
+		{
+		name = "default_tree.png",
+		backface_culling = true,
+		}
+	},
+	drawtype = "mesh",
+	mesh = "small_to_stick.obj",
+	paramtype = "light",
+	paramtype2 = "wallmounted",
+	groups = {oddly_breakable_by_hand=2},
+	selection_box = stick_nodebox,
+	collision_box = stick_nodebox
+})
---
+++ init.lua
@@ -0,0 +1,168 @@
+living_trees = {plants = {}}
+
+local modpath = minetest.get_modpath("living_trees")
+dofile(modpath.."/branches.lua")
+
+minetest.register_node("living_trees:roots", {
+	description = "Roots",
+	tiles = {"default_dirt.png^living_trees_roots.png"},
+	on_construct = function(pos)
+		local meta = minetest.get_meta(pos)
+		meta:set_string("lstring", "TTT[&12^34][^12&34]")--TT[+12-34][-12+34]")
+	end,
+	groups = {choppy = 3}
+})
+
+function opposite_dir(dir)
+	if dir % 2 == 0 then
+		return dir + 1
+	else
+		return dir - 1
+	end
+end
+
+function add_dir_to_pos(pos, dir)
+	if dir == 0 then
+		pos.y = pos.y + 1
+	elseif dir == 1 then
+		pos.y = pos.y - 1
+	elseif dir == 2 then
+		pos.x = pos.x + 1
+	elseif dir == 3 then
+		pos.x = pos.x - 1
+	elseif dir == 4 then
+		pos.z = pos.z + 1
+	elseif dir == 5 then
+		pos.z = pos.z - 1
+	end
+	return pos
+end
+
+function living_trees.register_tree(tree)
+	minetest.register_abm({
+		nodenames = tree.roots,
+		interval = 1.0, -- Run every n seconds
+		chance = 1, -- Select every 1 in n nodes
+		action = function(pos, node, active_object_count, active_object_count_wider)
+			local curpos = pos
+			local currot = {dir = 0, up = 3, left = 4}		-- In wallmounted numbers
+			local meta = minetest.get_meta(pos)
+			local lstr = meta:get_string("lstring")
+			local stack = {}
+			local skipbranch = false
+			for i = 1, #lstr do
+
+				local c = lstr:sub(i,i)
+				
+				if c == "]" and #stack then
+					currot = table.remove(stack)
+					curpos = table.remove(stack)
+					skipbranch = false
+				elseif skipbranch == true then
+				elseif c == "[" then
+					local savedpos = {}
+					for k,v in pairs(curpos) do
+						savedpos[k] = v
+					end
+					local savedrot = {}
+					for k,v in pairs(currot) do
+						savedrot[k] = v
+					end
+					table.insert(stack, savedpos)
+					table.insert(stack, savedrot)
+				elseif c == "T" then
+					add_dir_to_pos(curpos, currot.dir)
+					local name = minetest.get_node(curpos).name
+					if name == "air" then
+						minetest.set_node(curpos, {name="living_trees:branch_4", param2 = opposite_dir(currot.dir)})
+						skipbranch = true 
+					elseif name == "living_trees:branch_4" then
+						minetest.set_node(curpos, {name="living_trees:branch_3", param2 = opposite_dir(currot.dir)})
+					elseif name == "living_trees:branch_3" then
+						minetest.set_node(curpos, {name="living_trees:branch_2", param2 = opposite_dir(currot.dir)})
+					elseif name == "living_trees:branch_2" then
+						minetest.set_node(curpos, {name="living_trees:branch_1", param2 = opposite_dir(currot.dir)})
+					elseif name == "living_trees:branch_1" then
+						minetest.set_node(curpos, {name="default:tree", param2 = opposite_dir(currot.dir)})
+					elseif name ~= "default:tree" then
+						skipbranch = true
+					end
+				elseif c == "1" then
+					add_dir_to_pos(curpos, currot.dir)
+					local name = minetest.get_node(curpos).name
+					if name == "air" then
+						minetest.set_node(curpos, {name="living_trees:branch_4", param2 = opposite_dir(currot.dir)})
+						skipbranch = true
+					elseif name == "living_trees:branch_4" then
+						minetest.set_node(curpos, {name="living_trees:branch_3", param2 = opposite_dir(currot.dir)})
+					elseif name == "living_trees:branch_3" then
+						minetest.set_node(curpos, {name="living_trees:branch_2", param2 = opposite_dir(currot.dir)})
+					elseif name == "living_trees:branch_2" then
+						minetest.set_node(curpos, {name="living_trees:branch_1", param2 = opposite_dir(currot.dir)})
+					elseif name ~= "living_trees:branch_1" then
+						skipbranch = true
+					end
+				elseif c == "2" then
+					add_dir_to_pos(curpos, currot.dir)
+					local name = minetest.get_node(curpos).name
+					if name == "air" then
+						minetest.set_node(curpos, {name="living_trees:branch_4", param2 = opposite_dir(currot.dir)})
+						skipbranch = true
+					elseif name == "living_trees:branch_4" then
+						minetest.set_node(curpos, {name="living_trees:branch_3", param2 = opposite_dir(currot.dir)})
+					elseif name == "living_trees:branch_3" then
+						minetest.set_node(curpos, {name="living_trees:branch_2", param2 = opposite_dir(currot.dir)})
+					elseif name ~= "living_trees:branch_2" then
+						skipbranch = true
+					end
+				elseif c == "3" then
+					add_dir_to_pos(curpos, currot.dir)
+					local name = minetest.get_node(curpos).name
+					if name == "air" then
+						minetest.set_node(curpos, {name="living_trees:branch_4", param2 = opposite_dir(currot.dir)})
+						skipbranch = true
+					elseif name == "living_trees:branch_4" then
+						minetest.set_node(curpos, {name="living_trees:branch_3", param2 = opposite_dir(currot.dir)})
+					elseif name ~= "living_trees:branch_3" then
+						skipbranch = true
+					end
+				elseif c == "4" then
+					add_dir_to_pos(curpos, currot.dir)
+					local name = minetest.get_node(curpos).name
+					if name == "air" then
+						minetest.set_node(curpos, {name="living_trees:branch_4", param2 = opposite_dir(currot.dir)})
+						skipbranch = true
+					elseif name ~= "living_trees:branch_4" then
+						skipbranch = true
+					end
+				elseif c == "^" then
+					local temp = opposite_dir(currot.dir)
+					currot.dir = currot.up
+					currot.up = temp
+				elseif c == "&" then
+					local temp = opposite_dir(currot.up)
+					currot.up = currot.dir
+					currot.dir = temp
+				elseif c == "+" then
+					local temp = opposite_dir(currot.left)
+					currot.left = currot.dir
+					currot.dir = temp
+				elseif c == "-" then
+					local temp = opposite_dir(currot.dir)
+					currot.dir = currot.left
+					currot.left = temp
+				elseif c == "/" then
+					local temp = opposite_dir(currot.left)
+					currot.left = currot.up
+					currot.up = temp
+				elseif c == "*" then
+					local temp = opposite_dir(currot.up)
+					currot.up = currot.left
+					currot.left = temp
+				end
+			end
+		end
+	})
+end
+
+living_trees.register_tree({name = "dub", roots = {"living_trees:roots"} })
